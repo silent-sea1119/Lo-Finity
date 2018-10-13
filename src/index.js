3ab5dcbed11dd8e17f9fc154ad6cb7a4ab0086ce
silent-sea1119 <sandragonzales56t@gmail.com>
2018-10-12 20:40:51 -0400

    page routing and update .gitignore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './core/store';
+import history from './core/history';
+import Lofinity from './views/app';
+import registerServiceWorker from './registerServiceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+import './views/styles/styles.scss';
+
+const rootElement = document.getElementById('root');
+const store = configureStore();
+
+function render(Component) {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router history={history}>
+        <div>
+          <Component />
+        </div>
+      </Router>
+    </Provider>,
+    rootElement,
+  );
+}
+
+registerServiceWorker();
+render(Lofinity);
---
+++ src/views/pages/not-found-page/not-found-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function NotFoundPage() {
+  return (
+    <div>
+      <p>Page Not Found</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;

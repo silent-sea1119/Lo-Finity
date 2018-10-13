80f80ddac6eae7be7328c995e2eb28850e44048b
silent-sea1119 <sandragonzales56t@gmail.com>
2018-10-13 10:47:21 -0400

    cleaned up code

--- src/core/store.js
+++ src/core/store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 import reducers from './reducers';
 
+// Setup redux store but ended up not needing to use it
+
 export default function configureStore() {
   const store = createStore(
     reducers,
--- src/views/components/background-beat/polysynth.js
+++ src/views/components/background-beat/polysynth.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import {
   Delay,
   MoogFilter,
   Reverb,
-  Synth
+  Synth,
 } from 'react-music';
 
-const Polysynth = (props) => (
+const Polysynth = props => (
   <Delay>
     <Reverb>
       <MoogFilter bufferSize={4096}>
-      <Synth
-        type="square"
-        gain={0.15}
-        transpose={1}
-        steps={props.steps}
-      />
+        <Synth
+          type="square"
+          gain={0.15}
+          transpose={1}
+          steps={props.steps}
+        />
       </MoogFilter>
     </Reverb>
   </Delay>

0e3f37e248f4c5bce12f568340e88e3d80643177
silent-sea1119 <sandragonzales56t@gmail.com>
2018-10-13 05:42:44 -0400

    continually queue mp3s

--- server/mp3.js
+++ server/mp3.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require("http").Server(app);
 var fs = require("fs");
 var io = require("socket.io")(server);
+var mp3Duration = require("mp3-duration");
+var temporal = require("temporal");
 // const songNamespace = io.of("/song");
 
 app.use(express.static(`${__dirname}/html`));
@@ -11,12 +13,40 @@ server.listen(8000, function() {
   console.log('Listening at "/song" on port 8000\n');
 });
 
-app.get("/song", function(req, res) {
-  console.log("Got request for song");
-  var filename = __dirname + "/assets/songs/dave-brubeck-take-five.mp3";
+var files = fs.readdirSync("./assets/vocals/");
+
+function loop(res) {
+  var current = files.shift();
+  console.log("Playing: ", current);
+
+  if (current === undefined) {
+    files = fs.readdirSync("./assets/vocals/");
+    current = files.shift();
+  }
+
+  var filename = __dirname + "/assets/vocals/" + current;
   var stream = fs.createReadStream(filename);
 
-  while (true) {
-    stream.pipe(res);
-  }
+  mp3Duration(filename, function(err, duration) {
+    if (err) return console.log(err.message);
+
+    stream.pipe(res, { end: false });
+    temporal.delay(duration * 1000, function() {
+      console.log(duration * 1000, "later");
+      loop(res);
+    });
+  });
+}
+
+app.get("/song", function(req, res) {
+  console.log("Got request for song");
+
+  var filename = __dirname + "/assets/vocals/blind.mp3";
+  var stream = fs.createReadStream(filename);
+
+  stream.pipe(res, { end: false });
+
+  stream.on("end", function() {
+    loop(res);
+  });
 });

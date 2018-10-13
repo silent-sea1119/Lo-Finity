46760843410a92ee1ca868f6af1d957cd45a6959
silent-sea1119 <sandragonzales56t@gmail.com>
2018-10-13 10:27:19 -0400

    favicon

--- src/views/components/app-header/app-header.js
+++ src/views/components/app-header/app-header.js
@@ -8,7 +8,7 @@ const AppHeader = () => {
     <div>
       <div className="navBar">
         <div className="container">
-          <a href="https://jorbeatz.github.io/Lo-Finity/#/" className="navBar__title">Lo-finity <span className="navBar__subtitle">• homemade chill hip hop radio</span></a>
+          <a href="https://jorbeatz.github.io/Lo-Finity/#/" className="navBar__title">lo-finity <span className="navBar__subtitle">• homemade chill hip hop radio</span></a>
           <div className="navBar__actions">
             <p className="navBar__stack">React Redux | Python </p>
             <a href="https://github.com/Jorbeatz/Lo-Finity/tree/master"><img className="navBar__github" src={GithubLogo} height="30" alt="github logo"/></a>
